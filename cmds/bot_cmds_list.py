7ae38904d21521e9d53b93c17cbaf50a04dd44a5
Екатерина Александрова <aliensen65@gmail.com>
2025-05-11 14:11:06 +0300

    Админка: статистика

--- cmds/bot_cmds_list.py
+++ cmds/bot_cmds_list.py
@@ -3,6 +3,6 @@ from aiogram.types import BotCommand
 
 bot_cmds_list = [
     BotCommand(command='start', description='Рестарт бота'),
-BotCommand(command='admin', description='Админ-панель'),
+    BotCommand(command='admin', description='Админ-панель'),
     # BotCommand(command='help', description='Справка по работе бота'),
 ]
--- run.py
+++ run.py
@@ -1,25 +1,45 @@
 import asyncio
 import logging
 import sys
-from importlib import import_module
-from pathlib import Path
 
 from aiogram import Bot, Dispatcher, types
+from typing import Optional
 
 from admin.handlers.admin_handler import admin_router
 from cmds.bot_cmds_list import bot_cmds_list
 from database.engine import init_db, close_db
 from handlers.start_handler import start_router
-from config import TOKEN, PROPERTIES
+from config import TOKEN, PROPERTIES, ADMIN_CHAT_ID
 
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=TOKEN,
           default=PROPERTIES)
 
+async def send_to_chat(text: str, chat_id: Optional[int] = None):
+    """
+    Функция для отправки сообщений в чат админов
+    :param text: Текст сообщения
+    :param chat_id: ID чата (если None, используется CHAT_ID из переменных окружения)
+    """
+    target_chat_id = chat_id or ADMIN_CHAT_ID
+    if target_chat_id is None:
+        logging.error("Не указан chat_id для отправки сообщения")
+        return
+
+    try:
+        await bot.send_message(chat_id=target_chat_id, text=text)
+    except Exception as e:
+        logging.error(f"Ошибка отправки в чат {target_chat_id}: {e}")
+
 
 async def startup(dispatcher: Dispatcher):
     logger.info("Starting bot...")
+    if ADMIN_CHAT_ID is not None:
+        await send_to_chat(text="🔄 Бот был перезапущен!")
+    else:
+        logging.warning("ADMIN_CHAT_ID не установлен. "
+                        "Уведомление о перезапуске не отправлено.")
     try:
         await init_db()
     except Exception as e:
