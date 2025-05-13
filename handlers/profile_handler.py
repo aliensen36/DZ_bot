a3ee21c12a7dd812ed372e0542fa6f05c1f781c2
impuls13 <pavlo.w304@gmail.com>
2025-05-13 23:30:13 +0300

    add handlers my_data,my_subscriptions,my_bonuses

--- handlers/profile_handler.py
+++ handlers/profile_handler.py
@@ -27,3 +27,45 @@ async def my_data_handler(callback: CallbackQuery):
         reply_markup=await get_profile_inline_kb()
     )
     await callback.answer()
+
+# Хендлер для кнопки "Мои данные"
+@profile_router.callback_query(F.data == "my_data")
+async def my_data_handler(callback: CallbackQuery):
+    user_data_message = (
+        "📋 <b>Ваши данные:</b>\n\n"
+        "Фамилия, имя: <i>Иванов Иван</i>\n\n"
+        "Дата рождения: <i>01.01.2001</i>"
+    )
+    await callback.message.edit_text(
+        user_data_message,
+        reply_markup=await get_profile_inline_kb()
+    )
+    await callback.answer()
+
+    # Хендлер для кнопки "Мои подписки
+@profile_router.callback_query(F.data == "my_subscriptions")
+async def my_data_handler(callback: CallbackQuery):
+    user_data_message = (
+        "📋 <b>Ваши Подписки:</b>\n\n"
+        "Подписка: <i>ААА</i>\n\n"
+        "Подписка: <i>БББ</i>"
+    )
+    await callback.message.edit_text(
+        user_data_message,
+        reply_markup=await get_profile_inline_kb()
+    )
+    await callback.answer()
+
+    # Хендлер для кнопки "Мои Бонусы"
+@profile_router.callback_query(F.data == "my_bonuses")
+async def my_data_handler(callback: CallbackQuery):
+    user_data_message = (
+        "📋 <b>Ваши Бонусы:</b>\n\n"
+        "На посещение XXX: <i>100 Балов</i>\n\n"
+        "На посещение YYY: <i>305 Балов</i>"
+    )
+    await callback.message.edit_text(
+        user_data_message,
+        reply_markup=await get_profile_inline_kb()
+    )
+    await callback.answer()
