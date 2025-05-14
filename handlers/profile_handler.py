40975eea521224d7231868e3095ce00b757b9107
Екатерина Александрова <aliensen65@gmail.com>
2025-05-14 11:10:38 +0300

    Хендлеры "Мои подписки", "Мои бонусы"

--- handlers/profile_handler.py
+++ handlers/profile_handler.py
@@ -1,71 +1,102 @@
+import logging
+
 from aiogram import Router
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import Message, CallbackQuery
 from aiogram import F
 
 from keyboards.inline import get_profile_inline_kb
 
+
 profile_router = Router()
 
+
+# Хендлер для кнопки "Личный кабинет"
 @profile_router.message(F.text == "Личный кабинет")
 async def handle_profile(message: Message):
-    await message.answer(
-        "🔐 Ваш личный кабинет",
-        reply_markup=await get_profile_inline_kb()
-    )
+    try:
+        await message.answer(
+            "🔐 Ваш личный кабинет",
+            reply_markup=await get_profile_inline_kb()
+        )
+    except Exception as e:
+        logging.error(f"Error handling profile request: {e}", exc_info=True)
+        await message.answer("⚠️ Произошла ошибка, попробуйте позже")
 
 
 # Хендлер для кнопки "Мои данные"
 @profile_router.callback_query(F.data == "my_data")
 async def my_data_handler(callback: CallbackQuery):
-    user_data_message = (
-        "📋 <b>Ваши данные:</b>\n\n"
-        "Фамилия, имя: <i>Иванов Иван</i>\n\n"
-        "Дата рождения: <i>01.01.2001</i>"
-    )
-    await callback.message.edit_text(
-        user_data_message,
-        reply_markup=await get_profile_inline_kb()
-    )
-    await callback.answer()
+    try:
+        user_data_message = (
+            "🪪 <b>Основные данные:</b>\n\n"
+            "└ 🔖 <i>ФИО:</i> <code>Иванов Иван</code>\n"
+            "└ 🎂 <i>Дата рождения:</i> <code>01.01.2001</code>\n\n"
+            "📊 <b>Статистика:</b>\n\n"
+            "└ ⏱ <i>В системе:</i> <code>2 года 3 месяца</code>\n"
+            "└ 💫 <i>Последний вход:</i> <code>сегодня в 14:30</code>\n\n"
+        )
+        await callback.message.edit_text(
+            user_data_message,
+            reply_markup=await get_profile_inline_kb()
+        )
+        await callback.answer()
+    except TelegramBadRequest as e:
+        if "message is not modified" in str(e):
+            logging.debug(f"Ignored 'message not modified' for user {callback.from_user.id}")
+        else:
+            logging.error(f"TelegramBadRequest: {e}")
+            raise
+    finally:
+        await callback.answer()
 
-# Хендлер для кнопки "Мои данные"
-@profile_router.callback_query(F.data == "my_data")
-async def my_data_handler(callback: CallbackQuery):
-    user_data_message = (
-        "📋 <b>Ваши данные:</b>\n\n"
-        "Фамилия, имя: <i>Иванов Иван</i>\n\n"
-        "Дата рождения: <i>01.01.2001</i>"
-    )
-    await callback.message.edit_text(
-        user_data_message,
-        reply_markup=await get_profile_inline_kb()
-    )
-    await callback.answer()
 
-    # Хендлер для кнопки "Мои подписки
+# Хендлер для кнопки "Мои подписки
 @profile_router.callback_query(F.data == "my_subscriptions")
-async def my_data_handler(callback: CallbackQuery):
-    user_data_message = (
-        "📋 <b>Ваши Подписки:</b>\n\n"
-        "Подписка: <i>ААА</i>\n\n"
-        "Подписка: <i>БББ</i>"
-    )
-    await callback.message.edit_text(
-        user_data_message,
-        reply_markup=await get_profile_inline_kb()
-    )
-    await callback.answer()
+async def my_subscriptions_handler(callback: CallbackQuery):
+    try:
+        user_data_message = (
+            "🔔 <b>Ваши подписки:</b>\n\n"
+            "🍕 Акции Пиццерии «Сыр-р-р»\n\n"
+            "🎵 Афиша «Гластонберри»\n\n"
+            "📚 Новости лектория «Обсудим»\n\n"
+        )
+        await callback.message.edit_text(
+            user_data_message,
+            reply_markup=await get_profile_inline_kb()
+        )
+        await callback.answer()
+    except TelegramBadRequest as e:
+        if "message is not modified" in str(e):
+            logging.debug(f"Ignored 'message not modified' for user {callback.from_user.id}")
+        else:
+            logging.error(f"TelegramBadRequest: {e}")
+            raise
+    finally:
+        await callback.answer()
 
-    # Хендлер для кнопки "Мои Бонусы"
+
+# Хендлер для кнопки "Мои бонусы"
 @profile_router.callback_query(F.data == "my_bonuses")
-async def my_data_handler(callback: CallbackQuery):
-    user_data_message = (
-        "📋 <b>Ваши Бонусы:</b>\n\n"
-        "На посещение XXX: <i>100 Балов</i>\n\n"
-        "На посещение YYY: <i>305 Балов</i>"
-    )
-    await callback.message.edit_text(
-        user_data_message,
-        reply_markup=await get_profile_inline_kb()
-    )
-    await callback.answer()
+async def my_bonuses_handler(callback: CallbackQuery):
+    try:
+        user_data_message = (
+            "🎁 <b>Ваши бонусы</b>\n\n"
+            "☕ <b>15% скидка</b> на кофе в «Кофеин»\n"
+            "(действительна до 31.12.2025)\n\n"
+            "💪 <b>1 бесплатное посещение</b> фитнес-клуба «Жми»\n"
+            "(использовать до 15.11.2025)\n\n"
+        )
+        await callback.message.edit_text(
+            user_data_message,
+            reply_markup=await get_profile_inline_kb()
+        )
+        await callback.answer()
+    except TelegramBadRequest as e:
+        if "message is not modified" in str(e):
+            logging.debug(f"Ignored 'message not modified' for user {callback.from_user.id}")
+        else:
+            logging.error(f"TelegramBadRequest: {e}")
+            raise
+    finally:
+        await callback.answer()
