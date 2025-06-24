300f9a1a3066cd5aa5843f7fb48ee8fc0c40338f
Sayrrexe <sayrrexe@gmail.com>
2025-06-24 10:47:18 +0300

    добавлены новые хэндлеры для начисления баллов по номеру карты лояльности

--- data/url.py
+++ data/url.py
@@ -6,5 +6,6 @@ url_mailing = f"{base_url}/mailings/"
 url_loyalty = f"{base_url}/loyalty-cards/"
 url_resident = f"{base_url}/residents/"
 url_category = f"{base_url}/categories/"
+url_point_transactions_acrue = f'{base_url}/points-transactions/accrue/'
 
 
