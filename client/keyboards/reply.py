4c02abe437152e00145c59214f6f3cf3df013951
aliensen <aliensen@mail.ru>
2025-07-20 13:23:15 +0300

    ?mode=compact

--- client/keyboards/reply.py
+++ client/keyboards/reply.py
@@ -7,7 +7,7 @@ main_kb = ReplyKeyboardMarkup(
         [KeyboardButton(text="Карта лояльности")],
         [KeyboardButton(
             text="Открыть приложение",
-            web_app=WebAppInfo(url="https://design-zavod.tech/")
+            web_app=WebAppInfo(url="https://design-zavod.tech/?mode=compact")
         )
         ]
     ],
